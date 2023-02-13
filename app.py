626867f57b14470a8c0381927dcc4bc02d40747a
svkote <kotekofly@gmail.com>
2023-02-13 18:42:02 +0300

    fix

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 
 from config import Config
 from dao.model.user import User
+from implemented import user_service
 from setup_db import db
 from views.auth import auth_ns
 from views.directors import director_ns
@@ -22,26 +23,28 @@ def create_data(app, db):
     with app.app_context():
         db.create_all()
 
-        u1 = User(username="vasya", password="my_little_pony", role="user")
-        u2 = User(username="oleg", password="qwerty", role="user")
-        u3 = User(username="oleg", password="P@ssw0rd", role="admin")
+        users = [
+            {'username': "vasya", 'password': "my_little_pony", 'role': "user"},
+            {'username': "oleg", 'password': "qwerty", 'role': "user"},
+            {'username': "oleg", 'password': "P@ssw0rd", 'role': "admin"}
+        ]
 
-        with db.session.begin():
-            db.session.add_all([u1, u2, u3])
+        for user in users:
+            user_service.create(user)
 
 
 def register_extensions(app):
     db.init_app(app)
+    create_data(app, db)
+
+    with app.app_context():
+        db.create_all()
     api = Api(app)
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
     api.add_namespace(auth_ns)
     api.add_namespace(user_ns)
-    create_data(app, db)
-
-    with app.app_context():
-        db.create_all()
 
 
 app = create_app(Config())
