65d065fe021667049a5a4dad5dcbee4379720f86
Svetlana <36902132+svkote@users.noreply.github.com>
2023-02-28 23:11:31 +0300

    1

--- dao/model/user.py
+++ dao/model/user.py
@@ -6,8 +6,9 @@ from setup_db import db
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
+    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String)
+    username = db.Column(db.String)
     role = db.Column(db.String)
 
 
